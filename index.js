784474133d39524d07e8f73e4b3c501a37ecccbc
Joyce003 <jpirtle2348@gmail.com>
2024-01-10 14:06:34 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("I like pizza!");
+console.log("It's really good!");
+
+window.alert("I really love pizza!"); 
+
+//Declaration (var, let, const) = containers for storing data
+//Assignment (=assignment operator)
+
+// let age;
+// age = 26; //26 = number datatype
+
+age = age + 1;
+
+console.log(age);
+
+//string
+let firstName = "Joyce"; //stri ng
+let age = 26; //numbers
+//boolean
+let student = true; //or if they are graduated/not enrolled, then the value would be "false".
+
+age = age + 1;//console will log this as 266 because "1" added another 6 behind 26. 
+
+ console.log(firstName);
+ console.log(age);
+ console.log(student);
+
+ document.getElementById("p1").innerHTML = "Hello" + firstName; 
+ document.getElementById("p2").innerHTML = "You are"
