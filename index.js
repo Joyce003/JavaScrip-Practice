00fb278c8daf5a8382133a4bfc673f696d7fcd0a
Joyce003 <jpirtle2348@gmail.com>
2024-01-10 14:07:52 -0600

    Changed value of battery

--- index.js
+++ index.js
@@ -9,21 +9,53 @@ window.alert("I really love pizza!");
 // let age;
 // age = 26; //26 = number datatype
 
-age = age + 1;
+    age = age + 1;
 
-console.log(age);
+    console.log(age);
 
-//string
-let firstName = "Joyce"; //stri ng
-let age = 26; //numbers
-//boolean
-let student = true; //or if they are graduated/not enrolled, then the value would be "false".
+    //string
+    let firstName = "Joyce"; //stri ng
+    let age = 26; //numbers
+    //boolean
+    let student = true; //or if they are graduated/not enrolled, then the value would be "false".
 
-age = age + 1;//console will log this as 266 because "1" added another 6 behind 26. 
+    age = age + 1;//console will log this as 266 because "1" added another 6 behind 26. 
 
- console.log(firstName);
- console.log(age);
- console.log(student);
+    console.log(firstName);
+    console.log(age);
+    console.log(student);
 
- document.getElementById("p1").innerHTML = "Hello" + firstName; 
- document.getElementById("p2").innerHTML = "You are"
+    document.getElementById("p1").innerHTML = "Hello" + firstName; 
+    document.getElementById("p2").innerHTML = "You are"
+
+ //What I've Learned so far: 1/2/24-1/10/24
+ //number comparisons
+console.log(1 < 90) 
+console.log(1 < 235) //= true
+console.log(235 < 1)//= false
+//equality operator
+console.log(1 <= 3)
+//min <= max
+const min = 5;
+const max = 10;
+const result = min <= max;
+console.log(result); 
+//Check if batterLevel is less than or equal to operator <=.
+const batterLevel = 11;
+const low = batteryLevel <= 20;
+console.log("Low battery: " + low); // Low battery: true
+ //AND operator = requires all conditions to be true
+const isOnline = true;
+const isLoggedOn = true;
+console.log(isOnline && isLoggedOn);
+//Or operator = only 1 value needs to be true
+
+//Adding string values together
+"Followers: + 55";
+console.log("Followers" + "55");//result = Followers: 55 However, why not log consol.log (Followers: 55)? Why would we add 10 and 55 to give us 1055, why not just log 1055 to start with? Also, if we need this number to update based on user input, what variable would we use? Let? Var? Something else? Why would we need to manually put in numbers?
+//Another example
+const label = "Post:" + "13";
+console.log(label)
+ // = is variable assignment operator. What is variable assignment? Storing a value inside a variable.
+
+ 
