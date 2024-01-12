c5aa862623b836c43f68186476ac23896d73908f
Joyce003 <jpirtle2348@gmail.com>
2024-01-11 21:21:53 -0600

    changed isBatteryOn to is BatteryOff

--- index.js
+++ index.js
@@ -49,13 +49,17 @@ const isOnline = true;
 const isLoggedOn = true;
 console.log(isOnline && isLoggedOn);
 //Or operator = only 1 value needs to be true
+let isBatteryOn = true;
 
 //Adding string values together
 "Followers: + 55";
-console.log("Followers" + "55");//result = Followers: 55 However, why not log consol.log (Followers: 55)? Why would we add 10 and 55 to give us 1055, why not just log 1055 to start with? Also, if we need this number to update based on user input, what variable would we use? Let? Var? Something else? Why would we need to manually put in numbers?
+console.log("Followers" + "55");//result = Followers: 55. However, why not log consol.log (Followers: 55)? Why would we add 10 and 55 to give us 1055, why not just log 1055 to start with? Also, if we need this number to update based on user input, what variable would we use? Let? Var? Something else? Why would we need to manually put in numbers? //button funcion @likes document id likes box
 //Another example
 const label = "Post:" + "13";
 console.log(label)
  // = is variable assignment operator. What is variable assignment? Storing a value inside a variable.
+//Set up network via informational interviews with people you look up to
+//- I'm trying to learn about how to be like you
+//bootcamp route in 3 months //no college //boot camp grads/brats
 
  
